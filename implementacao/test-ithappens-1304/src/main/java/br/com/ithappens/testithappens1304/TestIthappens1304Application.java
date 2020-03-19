dc873b47a307ae6398f6d98a0855cafbd19b648e
Isaac Francis <isaacfrancis-cm@hotmail.com>
2020-03-19 15:43:56 -0300

    criação do projeto spring e adição do gitignore no projeto

---
+++ implementacao/test-ithappens-1304/src/main/java/br/com/ithappens/testithappens1304/TestIthappens1304Application.java
@@ -0,0 +1,13 @@
+package br.com.ithappens.testithappens1304;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestIthappens1304Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestIthappens1304Application.class, args);
+	}
+
+}
---
+++ implementacao/test-ithappens-1304/src/test/java/br/com/ithappens/testithappens1304/TestIthappens1304ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ithappens.testithappens1304;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestIthappens1304ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
